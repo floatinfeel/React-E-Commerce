7eacffc430509e64f4ae9bfb53a2461bae58059a
floatinfeel <dandythamrin@gmail.com>
2020-12-31 16:26:35 +0700

    add sign up user

--- server/app.js
+++ server/app.js
@@ -1,27 +1,43 @@
 const express = require('express')
 const cors = require('cors')
-const morgan = require('morgan')
+// const router = require('./routes/index')
+const authRoute = require('./routes/authRoute')
+const bodyParser = require('body-parser')
 
+const app = express()
+app.use(bodyParser.json())
+
+const morgan = require('morgan')
 require('dotenv').config({
-    patch: './config/index.env'
+    path: './config/index.env'
 })
 
-const port = 3001 || process.env.PORT
-const app = express()
+//mongodb
+const connectDB = require('./config/db')
+connectDB()
+
+
 
 app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'))
 app.use(cors())
 
+// routes
+app.use('/user/', authRoute)
+
 app.get('/', (req, res) => {
-    res.send('test route home page')
+  res.send('test route => home page')
+});
+
+// Page Not founded
+app.use((req, res) => {
+  res.status(404).json({
+    msg: 'Page not founded',
+  })
 })
 
-app.use((req, res)=>{
-    res.status(404).json({
-        msg: 'Page Not Found'
-    })
-})
+
+const port = process.env.PORT
 
 app.listen(port, ()=>{
     console.log(`Port running on ${port}`);
---
+++ server/routes/authRoute.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const router = express.Router()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+const gravatar = require('gravatar')
+//models
+const User = require('../models/User')
+//@route POST api/user/register
+//@desc Register user
+//@access Public
+
+router.post('/register', [
+   //validation
+   check('name', 'Name is require').not().isEmpty(),
+   check('email', 'Please insert a valid email').isEmail(),
+   check('password', 'Please enter a password with 6 or more characters').isLength({
+       min: 6
+   }) 
+
+], async (req, res) =>{ 
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    //get data from req form
+    const {name, email, password} = req.body
+
+    try {
+        //check if user already exist
+        let user = await User.findOne({email})
+
+        //if user already exists
+        if(user){
+            return res.status(400).json({
+                errors: [{msg: 'User already exists!'},],
+            })
+        }
+
+        //if not exists
+        const avatar = gravatar.url(email,{
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        //create user object
+        user = new User({name, email, avatar, password,})
+
+        //encrypt password
+        const salt = await bcrypt.genSalt(10)
+        //save password ---> use user password and salt to hash password
+        user.password = await bcrypt.hash(password, salt) 
+        //save in to database
+        await user.save()
+
+        //payload to generate token
+        const payload = {
+            user:{id: user.id}
+        }
+
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            {  expiresIn: 36000,},
+            (err, token) =>{
+                if(err) throw err
+                res.json({token})
+            }
+        )
+    } catch (error) {
+        console.log(error);
+        res.status(500).json('server error')
+    }
+ })
+
+module.exports = router
